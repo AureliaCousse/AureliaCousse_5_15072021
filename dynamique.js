66e458320430237878ec1dec0b533efa994cc0d7
AureliaCousse <80416325+AureliaCousse@users.noreply.github.com>
2021-07-28 15:24:11 +0200

    2021/07/28 15h25 footer

--- dynamique.js
+++ dynamique.js
@@ -134,6 +134,30 @@ function showRecipes(recipeTab) { /*fonction qui contient:*/
             recipeIngredients.appendChild(ingredientInfoList);
         }
 
+/*SEARCH BAR*/
+
+const searchinput = document.getElementById("searchInput");
+
+searchinput.addEventListener("keyup", function(){
+
+  const input = searchinput.value;
+
+  /* filter to get all words in title or description of the recipe containing caracters entered in search bar in lowercase or uppercase. */
+  const result = recipes.filter(item => item.name.toLocaleLowerCase().includes(input.toLocaleLowerCase())||item.description.toLocaleLowerCase().includes(input.toLocaleLowerCase()));
+
+  let suggestion = "";
+
+  if (input !=""){  /*if field input is not empty show result if not show nothing*/
+    result.forEach(resultItem => 
+      suggestion += `
+      <div class="suggestions">${resultItem.name}</div>`
+    )
+  }
+
+  document.getElementById("suggestions").innerHTML = suggestion;
+  
+})
+
 
 
 
