5be2ab60e3a2ed4c4e373a3217f38046b0beba79
AureliaCousse <80416325+AureliaCousse@users.noreply.github.com>
2021-10-10 19:03:40 +0200

    2021/10/10 19h

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 GIT HUB branches:
 
 * from branch dev-01 to -49 		-> workshop for ALGO_1
+  current final issue at dev-42
 
 * from branch dev-50 and forwards 	-> workshop for ALGO_2
--- script/utils.js
+++ script/utils.js
@@ -4,10 +4,23 @@
 //     return (strToNorm.toLowerCase()).normalize("NFD").replace(/\p{Diacritic}/gu, "");
 // } : REPLACED BY CLASS UTILS:
 class Utils{
+
     static normString(strToNorm){
         return (strToNorm.toLowerCase()).normalize("NFD").replace(/\p{Diacritic}/gu, "");
     }
 
+
+    static arrayContainString(array, value){
+        console.log(array)
+        console.log(value)
+        let result=false;
+        for (let i=0 ; i<value.length; i++){
+            if (array[i].toLowerCase().includes(value.toLowerCase())){
+                result=true;
+            }
+        }
+        return result;
+    }
     
     
     // renameElement(suggElement){
