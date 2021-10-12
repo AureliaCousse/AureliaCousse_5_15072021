8fd3894b045fab11eede00cd91a8d05152c3a446
AureliaCousse <80416325+AureliaCousse@users.noreply.github.com>
2021-10-12 17:55:58 +0200

    2021/10/12 update look recipe box

--- script/utils.js
+++ script/utils.js
@@ -1,13 +1,11 @@
-/*jshint esversion: 6 */ 
+/*jshint esversion: 6 */
 //When relying upon ECMAScript 6 features such as const, set this option so JSHint doesn't raise unnecessary warnings.
 
 // function normString(strToNorm){
 //     return (strToNorm.toLowerCase()).normalize("NFD").replace(/\p{Diacritic}/gu, "");
-// } : REPLACED BY CLASS UTILS:
+// } : REPLACED BY CLASS UTILS:   Utils.normString(elem)
 class Utils{
-
     static normString(strToNorm){
         return (strToNorm.toLowerCase()).normalize("NFD").replace(/\p{Diacritic}/gu, "");
     }
 }
-
